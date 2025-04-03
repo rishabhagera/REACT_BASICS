e40fea006412d8dc1cecd5389d536af5e17c0164
rishabhagera <rishabhupadhyay338@gmail.com>
2025-04-04 02:24:34 +0530

    Add files via upload

---
+++ customReact/customreact.js
@@ -0,0 +1,34 @@
+function customRedner(reactElement,mainContainer)
+{/*
+    // 1 
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    
+    mainContainer.appendChild(domElement)
+*/
+
+    //loop based code
+    const domElement =document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+
+    for (const prop in reactElement.props) {
+      if (prop === 'children') continue;
+        domElement.setAttribute(prop,reactElement.props.prop)
+    }
+    mainContainer.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href:'https://google.com',
+        target:"_blank"
+    },
+    children:'Click ME TO VISIT GOOGLE'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRedner(reactElement,mainContainer)
---
+++ src/chai.jsx
@@ -0,0 +1,8 @@
+function Chai()
+{
+    return(
+        <h3>YOUR TEA IS READY</h3>
+    )
+}
+
+export default Chai
